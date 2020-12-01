2e342641ca36bb37a609b75892be593ade57fefc
ahs-tkd <sha@doi.ics.keio.ac.jp>
2020-12-01 16:14:43 +0900

    メモ機能ひとまず

--- Assets/Scripts/InpurFanc.cs
+++ Assets/Scripts/InpurFanc.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using UnityEngine.UI;
 
 public class InpurFanc : MonoBehaviour
 {
+    public InputField inputField;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,7 +28,7 @@ public class InpurFanc : MonoBehaviour
     	//fileName = fileName + now.Year.ToString() + "_" + now.Month.ToString() + "_" + now.Day.ToString() + "__" + now.Hour.ToString() + "_" + now.Minute.ToString() + "_" + now.Second.ToString();
         fi = new FileInfo(path + "/memo.txt");
         sw = fi.AppendText();
-        sw.WriteLine("samplesample");
+        sw.WriteLine(inputField.text);
         sw.Flush();
         sw.Close();
         Debug.Log("Save Completed");

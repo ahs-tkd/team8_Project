15383b0a9fae2e7f6bc7b274802c3c4cfbc5a0c3
ahs-tkd <sha@doi.ics.keio.ac.jp>
2020-12-19 23:07:33 +0900

    カメラ機能　表示成功

---
+++ Assets/Scripts/CameraStart.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraStart : MonoBehaviour
+{
+
+	public RawImage RawImage;
+    WebCamTexture webCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // WebCamTextureのインスタンスを生成
+        webCam = new WebCamTexture();
+        //RawImageのテクスチャにWebCamTextureのインスタンスを設定
+        RawImage.texture = webCam;
+        //カメラ表示開始
+        webCam.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

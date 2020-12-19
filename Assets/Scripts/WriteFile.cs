c9137fa02e6b2732dfb061a75b232c7b90804213
倉重徹 <>
2020-12-20 00:06:18 +0900

    no message

--- Assets/Scripts/WriteFile.cs
+++ Assets/Scripts/WriteFile.cs
@@ -1,8 +1,8 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class WriteFile : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,5 +14,10 @@ public class NewBehaviourScript : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void Write(string content)
+    {
+
     }
 }

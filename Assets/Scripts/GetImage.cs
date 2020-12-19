7a52f0ca795bafe19982c43d610bc21929b246b2
ahs-tkd <sha@doi.ics.keio.ac.jp>
2020-12-20 05:03:38 +0900

    no message

---
+++ Assets/Scripts/GetImage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetImage : MonoBehaviour
+{
+	public RawImage _rawImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(GetImageFromPath(Application.persistentDataPath + @"/Data/icon_diary.png"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator GetImageFromPath(string path)
+    {
+        WWW www = new WWW(path);
+        yield return www;
+ 
+        _rawImage.texture = www.textureNonReadable;
+    }
+}

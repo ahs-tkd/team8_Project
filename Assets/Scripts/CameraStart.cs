abd593c9e383d2694e4308bcbe63e00c0de6c8f0
ahs-tkd <sha@doi.ics.keio.ac.jp>
2020-12-20 02:03:15 +0900

    カメラ画像保存

--- Assets/Scripts/CameraStart.cs
+++ Assets/Scripts/CameraStart.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
+using System;
 
 public class CameraStart : MonoBehaviour
 {
@@ -16,6 +18,11 @@ public class CameraStart : MonoBehaviour
         webCam = new WebCamTexture();
         //RawImageのテクスチャにWebCamTextureのインスタンスを設定
         RawImage.texture = webCam;
+
+        //90度カメラ回転
+        Vector3 angles = RawImage.GetComponent<RectTransform>().eulerAngles;
+        angles.z = -90;
+        RawImage.GetComponent<RectTransform>().eulerAngles = angles;
         //カメラ表示開始
         webCam.Play();
     }
@@ -24,5 +31,28 @@ public class CameraStart : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void OnClickShotButton()
+    {
+        // カメラ停止
+        webCam.Pause();
+
+        //データ保存
+        // Texture2Dを新規作成
+        Texture2D texture = new Texture2D(webCam.width, webCam.height, TextureFormat.ARGB32, false);
+        // カメラのピクセルデータを設定
+        texture.SetPixels(webCam.GetPixels());
+        // TextureをApply
+        texture.Apply();
+
+        // Encode
+        byte[] bin = texture.EncodeToJPG();
+        // Encodeが終わったら削除
+        //Object.Destroy(texture);
+
+        // ファイルを保存
+        DateTime now = DateTime.Now;
+        File.WriteAllBytes(Application.persistentDataPath + "/pic"+ now.Hour.ToString() + "-" + now.Minute.ToString() + "-" + now.Second.ToString() + "-" + ".jpg", bin);
     }
 }

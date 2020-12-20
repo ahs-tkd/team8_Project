4f83b8969e81376ce79fae205839d54fa3c60472
ahs-tkd <sha@doi.ics.keio.ac.jp>
2020-12-20 19:00:19 +0900

    複数画像表示成功

---
+++ Assets/Scripts/PicScroll.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PicScroll : MonoBehaviour
+{
+	public GameObject canvas;
+	public GameObject pref_rI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            GameObject rawImage = Instantiate(pref_rI, canvas.transform);
+            rawImage.GetComponent<RawImage>();
+            Transform Picture = GameObject.Find("GameObject").transform.GetChild(i);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

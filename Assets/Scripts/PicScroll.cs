c2c4c9a61f41ce723ba615ae377df27767961867
ahs-tkd <sha@doi.ics.keio.ac.jp>
2020-12-20 19:31:38 +0900

    配列導入

--- Assets/Scripts/PicScroll.cs
+++ Assets/Scripts/PicScroll.cs
@@ -8,14 +8,19 @@ public class PicScroll : MonoBehaviour
 	public GameObject canvas;
 	public GameObject pref_rI;
 
+	public string[] img_path;// = new string[2] {"/Data/icon_diary.png", "/Data/no_image.jpg"};
+	//int[] numbers = new int[3] { 4, 5, 6 }; 
+
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 10; i++)
+    	img_path = new string[2] {"/Data/icon_diary.png", "/Data/no_image.jpg"};
+        for (int i = 0; i < img_path.Length; i++)
         {
+
             GameObject rawImage = Instantiate(pref_rI, canvas.transform);
             rawImage.GetComponent<RawImage>();
-            Transform Picture = GameObject.Find("GameObject").transform.GetChild(i);
+            //Transform Picture = GameObject.Find("GameObject").transform.GetChild(i);
         }
     }
 
